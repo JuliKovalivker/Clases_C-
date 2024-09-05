993c8c55fff3cb97994248fa27f404b49c6a382f
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2024-09-05 01:49:31 -0300

    feat: clases (barely)

---
+++ Clases/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public class Persona(string nombre)
+            {
+                private string _nombre = nombre;
+            }
+
+        }
+    }
+}
